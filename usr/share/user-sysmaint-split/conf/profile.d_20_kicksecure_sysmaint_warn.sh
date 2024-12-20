a1f35d7caed4a03b81d299b20da21d677243c845
Aaron Rainbolt <arraybolt3@ubuntu.com>
2024-12-20 11:59:57 -0600

    Handle multiple shells in profile.d/20_kicksecure_sysmaint_warn.sh

--- usr/share/user-sysmaint-split/conf/profile.d_20_kicksecure_sysmaint_warn.sh
+++ usr/share/user-sysmaint-split/conf/profile.d_20_kicksecure_sysmaint_warn.sh
@@ -7,7 +7,12 @@
 . /usr/libexec/helper-scripts/get_colors.sh
 
 if [ "$(id -un)" = 'sysmaint' ]; then
-  ## 'echo -e' not supported by sh.
-  ## lintian: bash-term-in-posix-shell 'echo -e'
-  echo "${green}INFO${nocolor}] You are using the sysmaint account. This account has sudoers capabilities. Proceed with caution. See also: https://www.kicksecure.com/wiki/Sysmaint"
+   msg="[${green}INFO${nocolor}] You are using the sysmaint account. This account has sudoers capabilities. Proceed with caution. See also: https://www.kicksecure.com/wiki/Sysmaint"
+   msg_nocolor='[INFO] You are using the sysmaint account. This account has sudoers capabilities. Proceed with caution. See also: https://www.kicksecure.com/wiki/Sysmaint'
+   ## 'echo -e' not supported by sh.
+   if [ -n "$BASH_VERSION" ] || [ -n "$ZSH_VERSION" ]; then
+      echo -e "${msg}"
+   else
+      echo "${msg_nocolor}"
+   fi
 fi
