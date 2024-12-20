4637ead556c1946033b48a606d62432fc463a8bb
Patrick Schleizer <adrelanos@whonix.org>
2024-12-20 00:26:31 -0500

    fix lintian warnings

--- etc/profile.d/20_kicksecure-sysmaint-warn.sh
+++ etc/profile.d/20_kicksecure-sysmaint-warn.sh
@@ -3,8 +3,11 @@
 ## Copyright (C) 2024 - 2024 ENCRYPTED SUPPORT LP <adrelanos@whonix.org>
 ## See the file COPYING for copying conditions.
 
-source /usr/libexec/helper-scripts/get_colors.sh
+## 'source' is a bashism.
+. /usr/libexec/helper-scripts/get_colors.sh
 
 if [ "$(id -un)" = 'sysmaint' ]; then
-  echo -e "${green}INFO${nocolor}] You are using the sysmaint account. This account has sudoers capabilities. Proceed with caution. See also: https://www.kicksecure.com/wiki/Sysmaint"
+  ## 'echo -e' not supported by sh.
+  ## lintian: bash-term-in-posix-shell 'echo -e'
+  echo "${green}INFO${nocolor}] You are using the sysmaint account. This account has sudoers capabilities. Proceed with caution. See also: https://www.kicksecure.com/wiki/Sysmaint"
 fi
