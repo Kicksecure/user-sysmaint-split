63e6ca551a39a9ecf7d7dfd6992aca1c1a63c835
Patrick Schleizer <adrelanos@whonix.org>
2025-03-16 06:48:44 -0400

    end-of-options

--- usr/libexec/user-sysmaint-split/policy-rc.d
+++ usr/libexec/user-sysmaint-split/policy-rc.d
@@ -3,7 +3,7 @@
 ## Copyright (C) 2025 - 2025 ENCRYPTED SUPPORT LLC <adrelanos@whonix.org>
 ## See the file COPYING for copying conditions.
 
-kernel_cmdline="$(cat /proc/cmdline)"
+kernel_cmdline="$(cat -- /proc/cmdline)"
 
 if [[ "${kernel_cmdline}" =~ 'boot-role=sysmaint' ]]; then
   unit_name=""
@@ -15,13 +15,13 @@ if [[ "${kernel_cmdline}" =~ 'boot-role=sysmaint' ]]; then
     exit 0
   fi
   unit_name="$1"
-  IFS=' ' read -ra action_list <<< "$2"
+  IFS=' ' read -ra -- action_list <<< "$2"
 
   for action in "${action_list[@]}"; do
     case "${action}" in
       stop|force-stop|restart)
         if [ "${action}" = 'restart' ]; then
-          if [ "$(systemctl is-active "${unit_name}")" != 'active' ]; then
+          if [ "$(systemctl is-active -- "${unit_name}")" != 'active' ]; then
             exit 101
           fi
         fi
