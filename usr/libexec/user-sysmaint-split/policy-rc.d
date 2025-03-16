a9fba0997386b1b080d09d3ce31efcba97d1e0bf
Patrick Schleizer <adrelanos@whonix.org>
2025-03-16 07:06:21 -0400

    refactoring

--- usr/libexec/user-sysmaint-split/policy-rc.d
+++ usr/libexec/user-sysmaint-split/policy-rc.d
@@ -11,37 +11,39 @@
 
 kernel_cmdline="$(cat -- /proc/cmdline)"
 
-if [[ "${kernel_cmdline}" =~ 'boot-role=sysmaint' ]]; then
-  unit_name=""
-  action_list=()
-  if [ "$1" == "--quiet" ]; then
-    shift
-  fi
-  if [ "$1" == "--list" ]; then
-    exit 0
-  fi
-  unit_name="$1"
-  IFS=' ' read -r -a action_list <<< "$2"
-
-  for action in "${action_list[@]}"; do
-    case "${action}" in
-      stop|force-stop|restart)
-        ## action stop: permit
-        ## action force-stop: permit
-        ## action restart: if...
-        if [ "${action}" = 'restart' ]; then
-          if [ "$(systemctl is-active -- "${unit_name}")" != 'active' ]; then
-            ## action restart: If the systemd unit is NOT already running (active), deny (re)start.
-            exit 101
-          fi
-          ## action restart: If the systemd unit IS already running (active), permit (re)start.
-        fi
-        ;;
-      *)
-        ## action start: deny
-        ## action other (if any): deny
-        exit 101
-        ;;
-    esac
-  done
+if [[ ! "${kernel_cmdline}" =~ 'boot-role=sysmaint' ]]; then
+  exit 0
 fi
+
+unit_name=""
+action_list=()
+if [ "$1" == "--quiet" ]; then
+  shift
+fi
+if [ "$1" == "--list" ]; then
+  exit 0
+fi
+unit_name="$1"
+IFS=' ' read -r -a action_list <<< "$2"
+
+for action in "${action_list[@]}"; do
+  case "${action}" in
+    stop|force-stop|restart)
+      ## action stop: permit
+      ## action force-stop: permit
+      ## action restart: if...
+      if [ "${action}" = 'restart' ]; then
+        if [ "$(systemctl is-active -- "${unit_name}")" != 'active' ]; then
+          ## action restart: If the systemd unit is NOT already running (active), deny (re)start.
+          exit 101
+        fi
+        ## action restart: If the systemd unit IS already running (active), permit (re)start.
+      fi
+      ;;
+    *)
+      ## action start: deny
+      ## action other (if any): deny
+      exit 101
+      ;;
+  esac
+done
