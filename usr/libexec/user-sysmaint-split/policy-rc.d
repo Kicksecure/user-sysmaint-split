7dc3463d147b456eafcbbc9be08548af15486a4f
Patrick Schleizer <adrelanos@whonix.org>
2025-03-16 07:01:59 -0400

    comments

--- usr/libexec/user-sysmaint-split/policy-rc.d
+++ usr/libexec/user-sysmaint-split/policy-rc.d
@@ -3,6 +3,12 @@
 ## Copyright (C) 2025 - 2025 ENCRYPTED SUPPORT LLC <adrelanos@whonix.org>
 ## See the file COPYING for copying conditions.
 
+## systemd actions:
+## - 'stop' and 'force-stop' are always permitted.
+## - 'restart' is only allowed if the systemd unit is already running (active).
+## - 'start' is always denied.
+## - "other" actions (if any) are always denied.
+
 kernel_cmdline="$(cat -- /proc/cmdline)"
 
 if [[ "${kernel_cmdline}" =~ 'boot-role=sysmaint' ]]; then
@@ -20,13 +26,20 @@ if [[ "${kernel_cmdline}" =~ 'boot-role=sysmaint' ]]; then
   for action in "${action_list[@]}"; do
     case "${action}" in
       stop|force-stop|restart)
+        ## action stop: permit
+        ## action force-stop: permit
+        ## action restart: if...
         if [ "${action}" = 'restart' ]; then
           if [ "$(systemctl is-active -- "${unit_name}")" != 'active' ]; then
+            ## action restart: If the systemd unit is NOT already running (active), deny (re)start.
             exit 101
           fi
+          ## action restart: If the systemd unit IS already running (active), permit (re)start.
         fi
         ;;
       *)
+        ## action start: deny
+        ## action other (if any): deny
         exit 101
         ;;
     esac
