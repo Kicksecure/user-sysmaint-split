929364a78e23bca5934e55db279b7ab2194e1d87
Aaron Rainbolt <arraybolt3@ubuntu.com>
2025-03-25 17:36:30 -0500

    Only request boot mode features on Qubes R4.3

--- etc/qubes/post-install.d/21-qubes-persistent-mode-sysmaint.sh
+++ etc/qubes/post-install.d/21-qubes-persistent-mode-sysmaint.sh
@@ -3,6 +3,9 @@
 ## Copyright (C) 2025 - 2025 ENCRYPTED SUPPORT LLC <adrelanos@whonix.org>
 ## See the file COPYING for copying conditions.
 
-qvm-features-request boot-mode.kernelopts.sysmaint='boot-role=sysmaint systemd.unit=sysmaint-boot.target'
-qvm-features-request boot-mode.name.sysmaint='PERSISTENT mode SYSMAINT'
-qvm-features-request boot-mode.active='sysmaint'
+qubes_version="$(cat /usr/share/qubes/marker-vm | grep -v '^#')"
+if echo "${qubes_version}" | grep -q '^4.3' ; then
+   qvm-features-request boot-mode.kernelopts.sysmaint='boot-role=sysmaint systemd.unit=sysmaint-boot.target'
+   qvm-features-request boot-mode.name.sysmaint='PERSISTENT mode SYSMAINT'
+   qvm-features-request boot-mode.active='sysmaint'
+fi
