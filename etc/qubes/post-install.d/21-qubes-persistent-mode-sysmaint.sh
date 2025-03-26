e536fa5447fb51ae3a4572a50b38049a5bad783d
Patrick Schleizer <adrelanos@whonix.org>
2025-03-26 05:03:56 -0400

    future-proof for later Qubes versions

--- etc/qubes/post-install.d/21-qubes-persistent-mode-sysmaint.sh
+++ etc/qubes/post-install.d/21-qubes-persistent-mode-sysmaint.sh
@@ -4,7 +4,7 @@
 ## See the file COPYING for copying conditions.
 
 qubes_version="$(cat /usr/share/qubes/marker-vm | grep -v '^#')"
-if echo "${qubes_version}" | grep -q '^4.3' ; then
+if printf "%s\n" "${qubes_version}" | grep -q -e '^4.3' -e '^4.4' -e '^4.5' -e '^4.6' ; then
    qvm-features-request boot-mode.kernelopts.sysmaint='boot-role=sysmaint systemd.unit=sysmaint-boot.target'
    qvm-features-request boot-mode.name.sysmaint='PERSISTENT mode SYSMAINT'
    qvm-features-request boot-mode.active='sysmaint'
