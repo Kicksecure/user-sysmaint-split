d2f2d819d8e834f590644b4e742be6ab295b1c64
Patrick Schleizer <adrelanos@whonix.org>
2025-05-21 09:21:44 -0400

    improve /etc/profile.d scripts error handling

--- usr/share/user-sysmaint-split/conf/profile.d_20_sysmaint_warn.sh
+++ usr/share/user-sysmaint-split/conf/profile.d_20_sysmaint_warn.sh
@@ -6,8 +6,14 @@
 ## 'source' is a bashism.
 . /usr/libexec/helper-scripts/get_colors.sh || true >/dev/null 2>/dev/null
 
-if [ "$XDG_SESSION_TYPE" = "tty" ]; then
-   true "$0: INFO: Running inside tty. Stop."
+if [ ! "$XDG_SESSION_TYPE" = "tty" ]; then
+   true "$0: INFO: Not running inside tty. Stop."
+   return 0
+   exit 0
+fi
+
+if ! tty 2>/dev/null | grep -- "/dev/tty" >/dev/null 2>/dev/null ; then
+   true "$0: INFO: Not running inside tty. Stop."
    return 0
    exit 0
 fi
