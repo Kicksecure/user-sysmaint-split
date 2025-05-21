684d9156e8fc63e47126cd31a3544cf9a63c4275
Patrick Schleizer <adrelanos@whonix.org>
2025-05-21 08:52:31 -0400

    improve /etc/profile.d scripts error handling

--- usr/share/user-sysmaint-split/conf/profile.d_20_sysmaint_warn.sh
+++ usr/share/user-sysmaint-split/conf/profile.d_20_sysmaint_warn.sh
@@ -4,9 +4,9 @@
 ## See the file COPYING for copying conditions.
 
 ## 'source' is a bashism.
-. /usr/libexec/helper-scripts/get_colors.sh
+. /usr/libexec/helper-scripts/get_colors.sh >/dev/null 2>/dev/null
 
-if [ "$(id -un)" = 'sysmaint' ]; then
+if [ "$(id -un 2>&1)" = 'sysmaint' ]; then
    msg="[${green}INFO${nocolor}] You are using the sysmaint account. This account has sudoers capabilities. Proceed with caution. See also: https://www.kicksecure.com/wiki/Sysmaint"
    msg_nocolor='[INFO] You are using the sysmaint account. This account has sudoers capabilities. Proceed with caution. See also: https://www.kicksecure.com/wiki/Sysmaint'
    ## 'echo -e' not supported by sh.
