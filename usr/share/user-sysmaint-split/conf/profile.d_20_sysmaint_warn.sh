a30ababb14350076963b2bb8059a2ff1fab58956
Patrick Schleizer <adrelanos@whonix.org>
2025-05-21 09:10:12 -0400

    fix: show /etc/profile.d sysmaint account notification only if tty has been detected
    This is to avoid confusing non-login scripts with terminal output.

--- usr/share/user-sysmaint-split/conf/profile.d_20_sysmaint_warn.sh
+++ usr/share/user-sysmaint-split/conf/profile.d_20_sysmaint_warn.sh
@@ -6,6 +6,12 @@
 ## 'source' is a bashism.
 . /usr/libexec/helper-scripts/get_colors.sh || true >/dev/null 2>/dev/null
 
+if [ "$XDG_SESSION_TYPE" = "tty" ]; then
+   true "$0: INFO: Running inside tty. Stop."
+   return 0
+   exit 0
+fi
+
 if [ "$(id -un 2>&1)" = 'sysmaint' ]; then
    msg="[${green}INFO${nocolor}] You are using the sysmaint account. This account has sudoers capabilities. Proceed with caution. See also: https://www.kicksecure.com/wiki/Sysmaint"
    msg_nocolor='[INFO] You are using the sysmaint account. This account has sudoers capabilities. Proceed with caution. See also: https://www.kicksecure.com/wiki/Sysmaint'
